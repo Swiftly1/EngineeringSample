a9b791024e16e7b283d167024efeb1d5ac40b892
Swiftly1 <77643169+Swiftly1@users.noreply.github.com>
2022-08-06 17:55:11 +0200

    Update ContainersTests.cs

--- src/Tests/E2E/AST/Containers/ContainersTests.cs
+++ src/Tests/E2E/AST/Containers/ContainersTests.cs
@@ -7,6 +7,7 @@ using Text2Abstraction;
 using BenchmarkDotNet.Attributes;
 using AST.Trees.Statements.Typed;
 using AST.Trees.Expressions.Typed;
+using AST.Trees.Declarations.Typed;
 
 namespace Tests.E2E.AST.Containers
 {
@@ -33,11 +34,18 @@ namespace Tests.E2E.AST.Containers
             Assert.True(ast.Success);
             Assert.False(ast.Messages.Any());
 
-            //var container = ast.Data.Children[0].Children[0] as TypedContainer;
+            var container = ast.Data.Children[0].Children[0] as TypedContainerNode;
 
-            //Assert.NotNull(container);
+            Assert.NotNull(container);
 
-            throw new NotImplementedException();
+            Assert.Equal(2, container.Fields.Count);
+            Assert.True(container.Fields.Count == container.Children.Count);
+
+            Assert.Equal("a", container.Fields[0].Name);
+            Assert.Equal("b", container.Fields[1].Name);
+
+            Assert.Equal("int32", container.Fields[0].TypeInfo.Name);
+            Assert.Equal("int32", container.Fields[1].TypeInfo.Name);
         }
     }
 }
